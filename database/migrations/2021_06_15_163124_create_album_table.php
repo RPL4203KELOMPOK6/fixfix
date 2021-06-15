6c1a60f6bd7fa0031ecc3fa97544cda79a9253e4
kelompok6 <80332017+RPL4203KELOMPOK6@users.noreply.github.com>
2021-06-15 23:58:22 +0700

    fixfix

---
+++ database/migrations/2021_06_15_163124_create_album_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('album', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->string('penyanyi');
+            $table->string('gambar');
+            $table->integer('harga');
+            $table->longtext('deskripsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('album');
+    }
+}
